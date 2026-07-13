6c4fe6f675323cb48cce9f0c53d14ca6a405df39
rauatorres <rauatorres@gmail.com>
2026-07-13 17:20:09 -0300

    criando diretiva e seu comportamento

--- src/app/directives/safe-link.directive.ts
+++ src/app/directives/safe-link.directive.ts
@@ -2,9 +2,22 @@ import { Directive } from '@angular/core';
 
 @Directive({
   selector: 'a[appSafeLinkDirective]',
+  host: {
+    '(click)': 'onConfirmLeavePage($event)',
+  },
 })
 export class SafeLinkDirective {
   constructor() {
     console.log('SafeLink Directive is running');
+  }
+
+  onConfirmLeavePage(event: MouseEvent) {
+    const wantsToLeave = window.confirm('Deseja deixar a página?');
+
+    if (wantsToLeave) {
+      return;
+    }
+
+    event.preventDefault();
   }
 }
