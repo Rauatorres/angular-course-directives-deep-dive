4ee29db19170e78b7a0d6ac0898e6123b334df2f
rauatorres <rauatorres@gmail.com>
2026-07-14 11:44:42 -0300

    adicionando host directives

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,12 +3,13 @@ import { Component } from '@angular/core';
 import { AuthComponent } from './components/auth/auth.component';
 import { LearningResourcesComponent } from './components/learning-resources/learning-resources.component';
 import { AuthDirective } from './directives/auth.directive';
+import { LogDirective } from './directives/log.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [AuthComponent, LearningResourcesComponent, AuthDirective],
+  imports: [AuthComponent, LearningResourcesComponent, AuthDirective, LogDirective],
 })
 export class AppComponent {}
